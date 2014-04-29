091d8ba895c769c643ca87056e9d85af5d39dcfd
Alexander Kalkhof <entc@kalkhof.org>
2014-04-30 00:34:57 +0200

    fix

---
+++ libadbo/core/adbo_context_intern.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2010-2013 "Alexander Kalkhof" [email:adbo@kalkhof.org]
+ *
+ * This file is part of adbo framework (Advanced Database Objects)
+ *
+ * adbo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * adbo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with adbo. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ADBO_CONTEXT_INTERN_H
+#define ADBO_CONTEXT_INTERN_H 1
+
+#include <system/macros.h>
+#include <adbl_manager.h>
+
+#include "adbo_types.h"
+
+struct AdboContext_s
+{
+  
+  EcLogger logger;
+  
+  AdblManager adblm;
+  
+  AdboSubManager substitutes;
+  
+  AdboSchema schema;
+  
+};
+
+#endif
