25a45c23cfc987ce2ab956c6f416bd188f994462
Alexander Kalkhof <entc@kalkhof.org>
2016-08-24 23:40:27 +0200

    fix

--- libadbl/core/adbl_manager.c
+++ libadbl/core/adbl_manager.c
@@ -758,11 +758,17 @@ void adbl_sequence_release (AdblSequence** ptr)
 
 uint_t adbl_sequence_next (AdblSequence* self)
 {
-  if (isAssigned(self->pp->dbsequence_next))
+  if (isNotAssigned (self->pp))
   {
-      return self->pp->dbsequence_next(self->ptr);
+    return 0;
   }
-  return 0;  
+  
+  if (isNotAssigned (self->pp->dbsequence_next))
+  {
+    return 0;  
+  }
+
+  return self->pp->dbsequence_next (self->ptr);
 }
 
 /*------------------------------------------------------------------------*/
