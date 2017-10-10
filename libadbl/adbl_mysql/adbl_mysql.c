f3d2f3353f5c082679c437e0e9c70d963c55d26c
Alexander Kalkhof <alex@kalkhof.org>
2017-10-10 03:28:51 +0200

    update

--- libadbl/adbl_mysql/adbl_mysql.c
+++ libadbl/adbl_mysql/adbl_mysql.c
@@ -672,8 +672,6 @@ void* adblmodule_dbquery (void* ptr, AdblQuery* query)
   ret = adblmodule_dbquery_create (self, query, stmt, bv);
   
   // clean up
-  mysql_stmt_close (stmt);
-
   bindvars_destroy (&bv);
   
   ecmutex_unlock (self->mutex);
