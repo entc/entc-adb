b14e04f5a101fe49298450a621012dacc1dd8bfa
Alexander Kalkhof <alex@kalkhof.org>
2017-10-27 08:13:40 +0200

    initial

---
+++ entc/tests/tools/ut_lparser.c
@@ -0,0 +1,175 @@
+
+#include "tools/eclparser.h"
+#include "tests/ecenv.h"
+
+//=============================================================================
+
+typedef struct
+{
+  
+  EcLineParser slp;
+  
+  TestEnvContext tctx;
+  
+} TestStdlparserCtx;
+
+//---------------------------------------------------------------------------
+
+static void __STDCALL onLine (void* ptr, const char* line)
+{
+  TestStdlparserCtx* ctx = (TestStdlparserCtx*)ptr;
+  
+  testctx_assert (ctx->tctx, testctx_pop_tocomp (ctx->tctx, line), "line compare");
+}
+
+//---------------------------------------------------------------------------
+
+static void* __STDCALL test_stdlparser_init ()
+{
+  TestStdlparserCtx* ctx = (TestStdlparserCtx*)malloc (sizeof(TestStdlparserCtx));
+  
+  ctx->slp = eclineparser_create (onLine, ctx);
+  ctx->tctx = NULL;
+  
+  return ctx;
+}
+
+//---------------------------------------------------------------------------
+
+static void __STDCALL test_stdlparser_done (void* ptr)
+{
+  TestStdlparserCtx* ctx = (TestStdlparserCtx*)ptr;
+  
+  eclineparser_destroy (&(ctx->slp));
+  
+  free (ctx);
+}
+
+//---------------------------------------------------------------------------
+
+static int __STDCALL test_stdlparser_test1 (void* ptr, TestEnvContext tctx)
+{
+  TestStdlparserCtx* ctx = (TestStdlparserCtx*)ptr;
+  
+  // test1
+  const char* buf1 = "Hello\rWorld\rThanks!!";
+  
+  // should be the result in lines
+  testctx_push_string (tctx, "Hello");
+  testctx_push_string (tctx, "World");
+  testctx_push_string (tctx, "Thanks!!");
+  
+  ctx->tctx = tctx;
+  
+  EcLineParser slp = ctx->slp;
+  
+  eclineparser_parse (slp, buf1, 10);
+  eclineparser_parse (slp, buf1 + 10, 20);
+  
+  eclineparser_done (slp);
+  
+  return 0;
+}
+
+//---------------------------------------------------------------------------
+
+static int __STDCALL test_stdlparser_test2 (void* ptr, TestEnvContext tctx)
+{
+  TestStdlparserCtx* ctx = (TestStdlparserCtx*)ptr;
+  
+  // test1
+  const char* buf1 = "Hello\nWorld\n\n\nThanks!!\n";
+  
+  // should be the result in lines
+  testctx_push_string (tctx, "Hello");
+  testctx_push_string (tctx, "World");
+  testctx_push_string (tctx, NULL);
+  testctx_push_string (tctx, NULL);
+  testctx_push_string (tctx, "Thanks!!");
+  testctx_push_string (tctx, NULL);
+  
+  ctx->tctx = tctx;
+  
+  EcLineParser slp = ctx->slp;
+  
+  eclineparser_parse (slp, buf1, 10);
+  eclineparser_parse (slp, buf1 + 10, 20);
+  
+  eclineparser_done (slp);
+  
+  return 0;
+}
+
+//---------------------------------------------------------------------------
+
+static int __STDCALL test_stdlparser_test3 (void* ptr, TestEnvContext tctx)
+{
+  TestStdlparserCtx* ctx = (TestStdlparserCtx*)ptr;
+  
+  // test1
+  const char* buf1 = "Hello\r\nWorld\r\n\r\nThanks!!\r\n";
+  
+  // should be the result in lines
+  testctx_push_string (tctx, "Hello");
+  testctx_push_string (tctx, "World");
+  testctx_push_string (tctx, NULL);
+  testctx_push_string (tctx, "Thanks!!");
+  testctx_push_string (tctx, NULL);
+  
+  ctx->tctx = tctx;
+  
+  EcLineParser slp = ctx->slp;
+  
+  eclineparser_parse (slp, buf1, 10);
+  eclineparser_parse (slp, buf1 + 10, 20);
+  
+  eclineparser_done (slp);
+  
+  return 0;
+}
+
+//---------------------------------------------------------------------------
+
+static int __STDCALL test_stdlparser_test4 (void* ptr, TestEnvContext tctx)
+{
+  TestStdlparserCtx* ctx = (TestStdlparserCtx*)ptr;
+  
+  // test1
+  const char* buf1 = "Hello\r\nWorld\r\r\nThanks!!\n";
+  
+  // should be the result in lines
+  testctx_push_string (tctx, "Hello");
+  testctx_push_string (tctx, "World");
+  testctx_push_string (tctx, NULL);
+  testctx_push_string (tctx, "Thanks!!");
+  testctx_push_string (tctx, NULL);
+  
+  ctx->tctx = tctx;
+  
+  EcLineParser slp = ctx->slp;
+  
+  eclineparser_parse (slp, buf1, 10);
+  eclineparser_parse (slp, buf1 + 10, 20);
+  
+  eclineparser_done (slp);
+  
+  return 0;
+}
+
+//---------------------------------------------------------------------------
+
+int main(int argc, char* argv[])
+{
+  TestEnv te = testenv_create ();
+  
+  testenv_reg (te, "LineParser Test1", test_stdlparser_init, test_stdlparser_done, test_stdlparser_test1);
+  testenv_reg (te, "LineParser Test2", test_stdlparser_init, test_stdlparser_done, test_stdlparser_test2);
+  testenv_reg (te, "LineParser Test3", test_stdlparser_init, test_stdlparser_done, test_stdlparser_test3);
+  testenv_reg (te, "LineParser Test4", test_stdlparser_init, test_stdlparser_done, test_stdlparser_test4);
+  
+  testenv_run (te);
+  
+  return testenv_destroy (&te);
+}
+
+//---------------------------------------------------------------------------
---
+++ entc/tests/types/ut_eclist.c
@@ -0,0 +1,166 @@
+
+#include "types/eclist.h"
+#include "tests/ecenv.h"
+
+#include <stdio.h>
+
+//=============================================================================
+
+static int __STDCALL test_stdlist_onDestroy (void* ptr)
+{
+  free (ptr);
+  
+  return 0;
+}
+
+//---------------------------------------------------------------------------
+
+static void* __STDCALL test_stdlist_init ()
+{
+  return eclist_create (test_stdlist_onDestroy);
+}
+
+//---------------------------------------------------------------------------
+
+static void __STDCALL test_stdlist_done (void* ptr)
+{
+  EcList h = ptr;
+  
+  eclist_destroy (&h);
+}
+
+//---------------------------------------------------------------------------
+
+static int __STDCALL test_stdlist_test1 (void* ptr, TestEnvContext ctx)
+{
+  EcList h = ptr;
+  
+  for (int i = 0; i < 10; i++)
+  {
+    void* data = malloc(42);
+    
+    sprintf((char*)data, "hello world [%i]", i);
+    
+    eclist_push_back (h, data);
+  }
+  
+  eclist_clear (h);
+  
+  // prepare compare values
+  for (int i = 0; i < 5; i++)
+  {
+    void* data = malloc(42);
+    sprintf((char*)data, "hello world [%i]", i);
+    
+    eclist_push_back (h, data);
+    
+    // add for test to compare
+    testctx_push_string (ctx, (const char*)data);
+  }
+  
+  // forward
+  {
+    EcListCursor* cursor = eclist_cursor_create (h, 1);
+    
+    while (eclist_cursor_next (cursor))
+    {
+      void* data = eclist_data (cursor->node);
+      
+      // check ************
+      testctx_assert (ctx, testctx_pop_tocomp (ctx, (const char*)data), "comp #1");
+    }
+    
+    eclist_cursor_destroy (&cursor);
+  }
+  
+  // backwards
+  {
+    EcListCursor* cursor = eclist_cursor_create (h, 0);
+    
+    while (eclist_cursor_prev (cursor))
+    {
+      void* data = eclist_data (cursor->node);
+      
+      printf ("data: %s\n", data);
+    }
+    
+    eclist_cursor_destroy (&cursor);
+  }
+  
+  // combine
+  {
+    EcListCursor* cursor = eclist_cursor_create (h, 1);
+    
+    eclist_cursor_next (cursor);    // 0
+    eclist_cursor_next (cursor);    // 1
+    eclist_cursor_next (cursor);    // 2
+    
+    eclist_cursor_prev (cursor);    // 1
+    
+    void* data = eclist_data (cursor->node);
+    
+    printf ("comb: %s\n", data);
+    
+    eclist_cursor_destroy (&cursor);
+  }
+  
+  return 0;
+}
+
+//---------------------------------------------------------------------------
+
+static int __STDCALL test_stdlist_test2 (void* ptr, TestEnvContext tctx)
+{
+  EcList h = ptr;
+  
+  eclist_clear (h);
+  
+  // check ************
+  testctx_assert (tctx, eclist_size (h) == 0, "check size #1");
+  
+  for (int i = 0; i < 5; i++)
+  {
+    void* data = malloc(42);
+    eclist_push_back (h, data);
+  }
+  
+  // check ************
+  testctx_assert (tctx, eclist_size (h) == 5, "check size #2");
+  
+  {
+    EcListCursor cursor;
+    
+    eclist_cursor_init (h, &cursor, LIST_DIR_NEXT);
+    
+    eclist_erase (h, &cursor);
+    
+    // check ************
+    testctx_assert (tctx, eclist_size (h) == 4, "check size #3");
+    
+    while (eclist_cursor_next (&cursor))
+    {
+      eclist_erase (h, &cursor);
+    }
+  }
+  
+  // check ************
+  testctx_assert (tctx, eclist_size (h) == 0, "check size #4");
+  
+  return 0;
+}
+
+//=============================================================================
+
+int main(int argc, char* argv[])
+{
+  TestEnv te = testenv_create ();
+  
+  testenv_reg (te, "List Test1", test_stdlist_init, test_stdlist_done, test_stdlist_test1);
+  testenv_reg (te, "List Test2", test_stdlist_init, test_stdlist_done, test_stdlist_test2);
+  
+  testenv_run (te);
+  
+  return testenv_destroy (&te);
+}
+
+//---------------------------------------------------------------------------
