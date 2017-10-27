d94fa93ef7d9ab429540c2ec1042a656eafc941c
Alexander Kalkhof <alex@kalkhof.org>
2017-10-27 08:56:33 +0200

    initial

---
+++ entc/tests/system/ut_ecfile.c
@@ -0,0 +1,42 @@
+
+#include "types/ecmap.h"
+#include "tests/ecenv.h"
+
+#include <stdio.h>
+
+//=============================================================================
+
+static void* __STDCALL test_stdlist_init ()
+{
+  return NULL;
+}
+
+//---------------------------------------------------------------------------
+
+static void __STDCALL test_stdlist_done (void* ptr)
+{
+
+}
+
+//---------------------------------------------------------------------------
+
+static int __STDCALL test_stdlist_test1 (void* ptr, TestEnvContext ctx)
+{
+  
+  return 0;
+}
+
+//=============================================================================
+
+int main(int argc, char* argv[])
+{
+  TestEnv te = testenv_create ();
+  
+  testenv_reg (te, "Test1", test_stdlist_init, test_stdlist_done, test_stdlist_test1);
+  
+  testenv_run (te);
+  
+  return testenv_destroy (&te);
+}
+
+//---------------------------------------------------------------------------
