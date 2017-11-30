4e956f2d8959c9d4713f7d525686170544aee83e
Alexander Kalkhof <alex@kalkhof.org>
2017-11-30 07:53:54 +0100

    update

--- entc/tests/system/ut_ecaio.c
+++ entc/tests/system/ut_ecaio.c
@@ -203,6 +203,74 @@ static int __STDCALL test_ecaio_test3 (void* ptr, TestEnvContext ctx, EcErr err)
   return 0;
 }
 
+//---------------------------------------------------------------------------
+
+static int __STDCALL test_ecaio_test4_thread (void* ptr)
+{
+  EcErr err = ecerr_create();
+  
+  printf ("worker thread wait\n");
+  
+  ecaio_wait_abortOnSignal (ptr, FALSE, err);
+  
+  printf ("worker thread done\n");
+  
+  ecerr_destroy (&err);
+  
+  return 0;
+}
+
+//---------------------------------------------------------------------------
+
+static int __STDCALL test_ecaio_test4 (void* ptr, TestEnvContext ctx, EcErr err)
+{
+  EcAio aio = ptr;
+  
+  ecaio_init (aio, err);
+  
+  if (testctx_err (ctx, err))
+  {
+    
+  }
+  
+  {
+    int i;
+    
+    EcThread thread [10];
+    
+    for (i = 0; i < 10; i++)
+    {
+      thread [i] = ecthread_new();
+      
+      ecthread_start (thread [i], test_ecaio_test3_thread, aio);
+    }
+    
+    ece_sleep (1000);
+    
+#ifdef _WIN32
+    
+    GenerateConsoleCtrlEvent (CTRL_C_EVENT, 0);
+    
+#else
+    
+    printf ("send SIGINT\n");
+    
+    // this should not trigger
+    kill(getpid(), SIGINT);
+    
+#endif
+    
+    for (i = 0; i < 10; i++)
+    {
+      ecthread_join(thread [i]);
+      
+      ecthread_delete(&(thread [i]));
+    }
+  }
+  
+  return 0;
+}
+
 //=============================================================================
 
 int main(int argc, char* argv[])
@@ -211,7 +279,8 @@ int main(int argc, char* argv[])
   
   testenv_reg (te, "Single Thread", test_ecaio_init, test_ecaio_done, test_ecaio_test1);
   testenv_reg (te, "Multi Thread", test_ecaio_init, test_ecaio_done, test_ecaio_test2);
-  testenv_reg (te, "Signal Thread", test_ecaio_init, test_ecaio_done, test_ecaio_test3);
+  testenv_reg (te, "Signal TERM", test_ecaio_init, test_ecaio_done, test_ecaio_test3);
+  testenv_reg (te, "Signal INT", test_ecaio_init, test_ecaio_done, test_ecaio_test4);
   
   testenv_run (te);
   
