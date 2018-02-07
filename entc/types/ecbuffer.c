6344ec132857d4a000cddc4d7379aa0fea1b2af6
Alexander Kalkhof <alex@kalkhof.org>
2018-02-07 18:48:34 +0100

    cosmetics

--- entc/types/ecbuffer.c
+++ entc/types/ecbuffer.c
@@ -899,7 +899,7 @@ EcBuffer ecbuf_sha_256 (EcBuffer b1, EcErr err)
   int i = 0;
   for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
   {
-    sprintf((char*)(outb->buffer) + (i * 2), "%02x", hash[i]);
+    snprintf((char*)(outb->buffer) + (i * 2), 2, "%02x", hash[i]);
   }
 
   return outb;
