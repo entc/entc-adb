d59492475c5538794c9a8a0ef617a94a9eb1c1ac
Alexander Kalkhof <alex@kalkhof.org>
2018-02-22 11:57:37 +0100

    rm

--- push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git remote remove origin
-git remote add origin lobo@10.8.8.1:/home/lobo/mirror/repositories/entc-adb
-git push --all origin
--- push_github.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git remote remove origin
-git remote add origin https://github.com/entc/entc-adb
-git push -u origin ak_1_3_0
