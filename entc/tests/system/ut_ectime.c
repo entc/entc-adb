304c89e540497cbd869a002ee49d673fc7e8f7b6
Alexander Kalkhof <alex@kalkhof.org>
2018-05-15 15:47:12 +0200

    fix

---
+++ entc/tests/system/ut_ectime.c
@@ -0,0 +1,70 @@
+#include "tests/ecenv.h"
+#include "system/ectime.h"
+
+#include <stdio.h>
+
+//=============================================================================
+
+static int __STDCALL test_time1 (void* ptr, TestEnvContext ctx, EcErr err)
+{
+  EcTime t1;
+  
+  ectime_utc_time (&t1);
+  
+  printf ("T1: %i | %i\n", t1.sec, t1.msec);
+  
+  EcDate d1;
+  
+  ectime_utc_date (&d1);
+  
+  printf ("UTC Time: %i-%02i-%02i %02i:%02i:%02i.%03i\n", d1.year, d1.month, d1.day, d1.hour, d1.minute, d1.sec, d1.msec);
+  
+  ectime_local_date (&d1);
+  
+  printf ("LCL Time: %i-%02i-%02i %02i:%02i:%02i.%03i\n", d1.year, d1.month, d1.day, d1.hour, d1.minute, d1.sec, d1.msec);
+  
+  
+  EcBuffer buf = ecbuf_create (40);
+  
+  ectime_toGmtString (buf, &d1);
+  
+  printf ("GMT %s\n", buf->buffer);
+
+  ectime_toISO8601 (buf, &d1);
+
+  printf ("ISO8601 %s\n", buf->buffer);
+
+  ectime_toAlphaNum (buf, &d1);
+
+  printf ("AlphaNum %s\n", buf->buffer);
+
+  ectime_toString (buf, &d1);
+
+  printf ("String %s\n", buf->buffer);
+
+  ectime_toPrefix (buf, &d1);
+
+  printf ("Prefix %s\n", buf->buffer);
+
+  ectime_toPaddedTimestamp (buf, &d1);
+  
+  printf ("Padded %s\n", buf->buffer);
+
+  return ENTC_ERR_NONE;
+}
+
+//=============================================================================
+
+int main(int argc, char* argv[])
+{
+  TestEnv te = testenv_create ();
+  
+  testenv_reg (te, "Test1", NULL, NULL, test_time1);
+  
+  testenv_run (te);
+  
+  return testenv_destroy (&te);
+}
+
+//---------------------------------------------------------------------------
+
